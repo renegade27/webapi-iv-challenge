a09161d10ed75fec070de660acc9fe5dcbb34d1d
renegade27 <foemaster@live.com>
2019-05-09 10:08:08 -0500

    Added cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const middleware = require('./middlewares');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,9 @@ const server = express();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
+
 server.use(express.json());
+server.use(cors());
 server.use(middleware.capital);
 server.use('/api', postRoutes);
 server.use('/api', userRoutes);
